bfde035a34db21479c64ae381d15850c2afd7baf
Alison Law <alisparklaw@gmail.com>
2019-12-10 15:27:44 +0000

    Further config, not broken

--- todo-app/src/components/TodoForm.js
+++ todo-app/src/components/TodoForm.js
@@ -1,12 +1,38 @@
 import React, { useReducer } from "react";
-import { reducer } from "../reducers/reducer";
+import {
+  reducer,
+  initialState,
+  INPUT_CHANGE,
+  SUBMIT_TODO,
+  TOGGLE_TODO,
+  REMOVE_TODO
+} from "../reducers/reducer";
 
 const TodoForm = props => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const onValueChange = e => {
+    const { item, value } = e.target;
+    dispatch({
+      type: INPUT_CHANGE,
+      payload: { item, value }
+    });
+  };
+  const onFormSubmit = event => {
+    event.preventDefault();
+    dispatch({ type: SUBMIT_TODO });
+  };
+  //   , payload: { item, completed, id }
   return (
     <div>
       <div>
         <form>
-          <input type="text" placeholder="Add todo" value={props.todoValue} />
+          <input
+            type="text"
+            placeholder="Add todo"
+            value={state.item}
+            onChange={onValueChange}
+          />
           <button type="submit">Submit Todo</button>
         </form>
       </div>
